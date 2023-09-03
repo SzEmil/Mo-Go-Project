aab8a0c7d3cb341c6e0d6ce3868c366d835ddf0e
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2023-09-03 17:23:25 +0200

    Responsive design
    responsive design for all sections + mobile menu js and design

--- js/active-link.js
+++ js/active-link.js
@@ -1,11 +1,24 @@
-const listItems = document.querySelectorAll('.nav-list__link');
-const homeLink = document.querySelector('#home');
+const listItems = document.querySelectorAll('.navigation__link');
+const menuItems = document.querySelectorAll('.mobile-nav-list__link');
+const homeLinks = document.querySelectorAll('#home');
 
-homeLink.classList.add('activeLink');
+homeLinks.forEach(link => {
+  link.classList.add('activeLink');
+});
 
 listItems.forEach(item => {
   item.addEventListener('click', () => {
     listItems.forEach(li => {
+      li.classList.remove('activeLink');
+    });
+
+    item.classList.add('activeLink');
+  });
+});
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    menuItems.forEach(li => {
       li.classList.remove('activeLink');
     });
 
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,28 +1,47 @@
 (() => {
-  const mobileMenu = document.querySelector(".js-menu-container");
-  const openMenuBtn = document.querySelector(".js-open-menu");
-  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
     const isMenuOpen =
-      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenu.classList.toggle("is-open");
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
-      ? "disableBodyScroll"
-      : "enableBodyScroll";
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
-  openMenuBtn.addEventListener("click", toggleMenu);
-  closeMenuBtn.addEventListener("click", toggleMenu);
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
-    mobileMenu.classList.remove("is-open");
-    openMenuBtn.setAttribute("aria-expanded", false);
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+const listItems = document.querySelectorAll('.mobile-nav-list__link');
+console.log(listItems);
+const mobileMenu = document.querySelector('.mobile-menu');
+
+mobileMenu.addEventListener('click', e => {
+  if (e.target.tagName === 'A') {
+    return;
+  } else {
+    mobileMenu.classList.remove('is-open');
+  }
+});
+
+listItems.forEach(item => {
+  item.addEventListener('click', () => {
+    setTimeout(() => {
+      mobileMenu.classList.remove('is-open');
+    }, 100);
+  });
+});
